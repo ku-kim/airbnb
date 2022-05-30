1aad1682cd2c8bbf28024cb47a045694418895bd
sanghyeok-kim <slsnsi1212@gmail.com>
2022-05-30 11:28:20 +0900

    feat: Repository 구현
    - 모든 Repository들이 부모로 가질 상위 클래스 NetworkRepository (decdoe 메소드를 포함)
    - SearchHome에서 사용할 Repository 프로토콜 (SearchHomeRepository)
    - SearchHomeRepository을 채택해서 구현한 SearchHomeRepositoryImpl
    
    jeremy0405/airbnb/#9

---
+++ iOS/AirbnbApp/AirbnbApp/Source/Repository/SearchHomeRepository.swift
@@ -0,0 +1,18 @@
+//
+//  SearchHomeRepository.swift
+//  AirbnbApp
+//
+//  Created by 김상혁 on 2022/05/29.
+//
+
+import Foundation
+
+protocol SearchHomeRepository {
+    
+    func requestNearDestination(latitude: Double,
+                                longtitude: Double,
+                                completion: @escaping (Result<SearchHomeEntity.NearCity, NetworkError>) -> Void)
+    func requestHeroBanner(completion: @escaping (Result<SearchHomeEntity.HeroBanner, NetworkError>) -> Void)
+    
+    func requestTheme()
+}
