a4bb6b39f0fd441103ffcfdd262e836749ce3567
hsjang <hsjang0405@gmail.com>
2022-05-31 11:41:20 +0900

    build: 더미데이터 data.sql, schema.sql 생성, application.yml 파일 변경
    - event 테이블 생성
    - place 테이블 생성
    - event, place 더미데이터 추가

---
+++ BE/src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+create table event
+(
+    id          bigint          not null    auto_increment      comment '이벤트 PK',
+    title       varchar(255)                                    comment '이벤트 제목',
+    description varchar(255)                                    comment '이벤트 설명',
+    image_url   varchar(1000)                                    comment '이벤트 이미지 URL',
+    is_main     boolean         not null                        comment '메인이벤트 판단',
+    primary key (id)
+);
+
+
+create table place
+(
+    id        bigint        not null        auto_increment      comment '여행지 PK',
+    title     varchar(255)                                      comment '인기 여행지 이름',
+    image_url varchar(1000)                                      comment '여행지 이미지 URL',
+    lat       double                                            comment '인기 여행지 위도',
+    lng       double                                            comment '인기 여행지 경도',
+    primary key (id)
+);
