44a7d3701b3cae8e0e98ee0710968e29bba61473
hsjang <hsjang0405@gmail.com>
2022-05-31 12:12:52 +0900

    fix: PlaceRepository 에서 쓸모없는 주석, 로직 제거

--- BE/src/main/java/team13/kuje/airbnb/repository/PlaceRepository.java
+++ BE/src/main/java/team13/kuje/airbnb/repository/PlaceRepository.java
@@ -14,23 +14,6 @@ public class PlaceRepository {
 
 	private final EntityManager entityManager;
 
-//	public List<Place> findByPosition(Position inputPosition) {
-//		return entityManager.createQuery("select p from Place p", Place.class)
-//			.getResultList();
-//	}
-
-	public List<Place> findByPosition(double minLat, double minLng, double maxLat, double maxLng) {
-		return entityManager.createQuery(
-				"select p from Place p "
-					+ "where p.position.lat between :minLat and :maxLat "
-					+ "and p.position.lng between :minLng and :maxLng", Place.class)
-			.setParameter("minLat", minLat)
-			.setParameter("maxLat", maxLat)
-			.setParameter("minLng", minLng)
-			.setParameter("maxLng", maxLng)
-			.getResultList();
-	}
-
 	public List<Place> findByPosition(Position southWestPosition, Position northEastPosition) {
 		return entityManager.createQuery(
 				"select p from Place p "
