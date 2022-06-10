e6c41a9927754ba74e4a2d975a42294eb3f7d6e8
sanghyeok-kim <slsnsi1212@gmail.com>
2022-06-10 19:25:31 +0900

    feat: FilteringVM에 하위 VM (달력VM, 가격범위VM, 인원수VM) 관련 기능 구현
    - 각각의 하위 VM을 초기화하여 프로퍼티로 가짐
    - 각각의 하위 VM에서 들어오는 값을 bind하여 자신의 테이블 뷰에 그리도록 accept함

--- iOS/AirbnbApp/AirbnbApp/Source/Present/Filtering/FilteringViewModel.swift
+++ iOS/AirbnbApp/AirbnbApp/Source/Present/Filtering/FilteringViewModel.swift
@@ -10,26 +10,65 @@ import MapKit
 
 class FilteringViewModel {
     
-    let totalHeadCountViewModel = HeadCountViewModel()
+    let yearViewModel = YearViewModel()
+    let priceRangeViewModel = PriceRangeViewModel()
+    let headCountViewModel = HeadCountViewModel()
     
-    let updatedTotalHeadCount = PublishRelay<Int>()
-    //4가지 조건 다 채워지면 '다음'버튼 활성화
-    //다음버튼이 눌러지면, 4가지 조건을 서버로 보내서, 받아온 정보에 대한 ViewModel을 만들어서 다음 VC를 Push
+    lazy var conditionMap: [FilteringCondition: String] = [:]
     
     let location: MKLocalSearchCompletion
     
     let loadLoacationName = PublishRelay<Void>()
     let loadedLocationName = PublishRelay<String>()
     
+    let updatedSchedule = PublishRelay<String>()
+    let updatedPriceRange = PublishRelay<String>()
+    let updatedTotalCount = PublishRelay<String>()
+    
     init(location: MKLocalSearchCompletion) {
         self.location = location
         
         loadLoacationName.bind { [weak self] in
+            self?.conditionMap[.location] = location.title
             self?.loadedLocationName.accept(location.title)
         }
         
-        totalHeadCountViewModel.updatedTotalCount.bind { [weak self] value in
-            self?.updatedTotalHeadCount.accept(value)
+        yearViewModel.updatedSchedule.bind { [weak self] date in
+            guard let checkInDate = date.first,
+                  let checkOutDate = date.last else { return }
+            
+            let checkInString = "\(checkInDate.month.getMonth())월 \(checkInDate.day.getDay())일"
+            let checkOutString = "\(checkOutDate.month.getMonth())월 \(checkOutDate.day.getDay())일"
+            
+            let schedule = [checkInString, checkOutString].joined(separator: .Separator.schedule)
+            
+            self?.updatedSchedule.accept(schedule)
+        }
+        
+        priceRangeViewModel.updatedPriceCondition.bind { [weak self] (min, max) in
+            let formattedMinPrice = NumberFormatter.toPriceUnit(from: Int(min)) ?? ""
+            let formattedMaxPrice = NumberFormatter.toPriceUnit(from: Int(max)) ?? ""
+            let priceRange = "₩\(formattedMinPrice) - ₩\(formattedMaxPrice)+"
+            
+            self?.priceRangeViewModel.updatedMinPrice.accept(formattedMinPrice)
+            self?.priceRangeViewModel.updatedMaxPrice.accept(formattedMaxPrice)
+            self?.updatedPriceRange.accept(priceRange)
+        }
+        
+        headCountViewModel.updatedTotalCount.bind { [weak self] totalCount in
+            guard let totalCount = totalCount else { return }
+            let totalAdult = totalCount[Age.adult.index]
+            let totalKid = totalCount[Age.kid.index]
+            let totalToddler = totalCount[Age.toddler.index]
+            
+            var guests = ["게스트 \(totalAdult + totalKid)명"]
+            
+            if totalToddler != 0 {
+                guests.append("유아 \(totalToddler)명")
+            }
+            
+            let guest = guests.joined(separator: .Separator.guest)
+            self?.updatedTotalCount.accept(guest)
         }
     }
     
